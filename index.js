a7f48ec8ed35d05d39622c27d442508d1497b5c4
Aidan Margo <aidanmargo@gmail.com>
2021-09-21 10:37:19 -0500

    commit tree bac2816372c556d310be777e21f28de12638eb07

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+
+
+
+const returnFirstTwoDrivers = function (drivers){ 
+  return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function (drivers){
+  return drivers.slice(2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(fare) {
+  function multiplier(fare){ 
+    return fare*5
+  }; 
+  return multiplier
+}
+
+const fareDoubler = (fare) => fare*2 
+const fareTripler = (fare) => fare*3
+
+const selectDifferentDrivers = (drivers, selected) => {
+
+  if (selected === returnFirstTwoDrivers) {
+      return drivers.slice(0 , 2)
+    } else {
+      return drivers.slice(2)
+  };
+}
